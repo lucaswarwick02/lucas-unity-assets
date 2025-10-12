bba4c2a0810668220afa7e58ed3e35c4d5059eb1
Lucas Warwick <lucaswarwick02@gmail.com>
2025-10-12 19:15:19 +0100

    AnimationLoop now using single variable for target. Also update README with new structure

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# arcadian-assets
+# Arcadian Assets
+
+A colelction of shared Unity tools and utilities for internal projects.
+
+---
+
+## Setup Guide
+
+1. Open **Edit -> Project Settings -> Project Manager**
+2. Under **Scoped Registries**, add the following:
+```json
+"scopedRegistries": [
+    {
+        "name": "package.openupm.com",
+        "url": "https://package.openupm.com",
+        "scopes": [
+            "com.solidalloy",
+            "com.openupm",
+            "org.nuget"
+        ]
+    }
+],
+```
+3. Open **Window -> Package Manager**
+4. Add https://github.com/lucaswarwick02/arcadian-assets.git as a Git URL
+
+---
+
+## Features
+
+### Table of Contents
+
+Below is a list of the different submodules:
+- [Animation](#animation)
+
+### Animation
+
+`AnimationLoop`
+
+A reusable Unity component for quickly animating a sequence of sprites on a `SpriteRenderer` or `UI.Image`. Useful for creating looping or one-off animations (like effect, UI icons, or simple character aniamtions) without needing an Animator Controller, giving lightweight, scriptable control over timing, looping, and destruction on completion.
+
+Example Usage:
+```c#
+using Arcadian.Animation;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    public AnimationLoop animationLoop;
+
+    void Start()
+    {
+        // Play the animation
+        animationLoop.Play();
+
+        // Subscribe to finish event
+        animationLoop.onAnimationFinished += () => Debug.Log("Animation finished!");
+    }
+
+    void Update()
+    {
+        // Stop animation on space press
+        if (Input.GetKeyDown(KeyCode.Space))
+            animationLoop.Stop();
+    }
+}
+
+```
--- Runtime/Arcadian/Animation/AnimationLoop.cs
+++ Runtime/Arcadian/Animation/AnimationLoop.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Arcadian.Animation
 {
     /// <summary>
@@ -10,11 +14,11 @@ namespace Arcadian.Animation
     /// </summary>
     public class AnimationLoop : MonoBehaviour
     {
-        public SpriteRenderer spriteRenderer;
-        public Image image;
+        public Component target;
+        
         public float frameDelay = 0.125f;
         public Sprite[] sprites;
-        
+
         public bool destroyOnFinish;
         public bool playOnStart;
         public bool loop;
@@ -23,6 +27,18 @@ namespace Arcadian.Animation
         public event Action onAnimationFinished;
 
         private Coroutine _play;
+
+        public Component Target
+        {
+            get => target;
+            set
+            {
+                if (value is SpriteRenderer || value is Image || value == null)
+                {
+                    target = value;
+                }
+            }
+        }
 
         private void Awake()
         {
@@ -47,38 +63,72 @@ namespace Arcadian.Animation
 
         public void Stop()
         {
-            if (_play != null)
-            {
-                StopCoroutine(_play);
-                _play = null;
-            }  
+            if (_play == null) return;
+
+            StopCoroutine(_play);
+            _play = null;
         }
 
         private IEnumerator PlayCoroutine()
         {
-            foreach (var t in sprites)
+            do
             {
-                SetSprite(t);
-                if (useUnscaledTime)
+                foreach (var sprite in sprites)
                 {
-                    yield return new WaitForSecondsRealtime(frameDelay);
-                }
-                else
-                {
-                    yield return new WaitForSeconds(frameDelay);
-                }
-            }
+                    SetSprite(sprite);
+                    yield return useUnscaledTime ? new WaitForSecondsRealtime(frameDelay) : new WaitForSeconds(frameDelay);
 
-            onAnimationFinished?.Invoke();
-            
-            if (destroyOnFinish) Destroy(gameObject);
-            if (loop) _play = StartCoroutine(PlayCoroutine());
+                    onAnimationFinished?.Invoke();
+
+                    if (destroyOnFinish)
+                    {
+                        Destroy(gameObject);
+                        yield break;
+                    }
+                }
+            } while (loop);
         }
-        
+
         private void SetSprite(Sprite sprite)
         {
-            if (spriteRenderer) spriteRenderer.sprite = sprite;
-            if (image) image.sprite = sprite;
+            if (target is SpriteRenderer sr) sr.sprite = sprite;
+            if (target is Image img) img.sprite = sprite;
         }
     }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(AnimationLoop))]
+    public class AnimationLoopEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            var script = serializedObject.FindProperty("m_Script");
+            EditorGUI.BeginDisabledGroup(true); // Script field is read-only
+            EditorGUILayout.PropertyField(script);
+            EditorGUI.EndDisabledGroup();
+
+            var targetProp = serializedObject.FindProperty("target");
+            EditorGUI.BeginChangeCheck();
+            var obj = EditorGUILayout.ObjectField("Target", targetProp.objectReferenceValue, typeof(Component), true);
+
+            if (obj == null || obj is SpriteRenderer || obj is Image)
+                targetProp.objectReferenceValue = obj;
+            else
+                EditorGUILayout.HelpBox("Target must be a SpriteRenderer or Image.", MessageType.Warning);
+
+            // Draw remaining fields manually
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("frameDelay"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("sprites"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("destroyOnFinish"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("playOnStart"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("loop"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("useUnscaledTime"));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
+
 }
