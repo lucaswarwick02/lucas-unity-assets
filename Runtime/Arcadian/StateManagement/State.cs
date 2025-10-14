2709f715b6894ca233d7511fa19b338919ded4a4
Lucas Warwick <lucaswarwick02@gmail.com>
2025-10-14 12:22:33 +0100

    Improve StateManagement documentation + move to submodule README.md

--- Runtime/Arcadian/StateManagement/State.cs
+++ Runtime/Arcadian/StateManagement/State.cs
@@ -1,9 +1,18 @@
 ﻿namespace Arcadian.StateManagement
 {
+    /// <summary>
+    /// Abstract State with simple abstract functions for entering and exiting it's state.
+    /// </summary>
     public abstract class State
     {
+        /// <summary>
+        /// Ran when the state machine switches to this state.
+        /// </summary>
         public virtual void StartState() { }
 
+        /// <summary>
+        /// Ran when the state machine exits this state.
+        /// </summary>
         public virtual void EndState() { }
     }
 }
